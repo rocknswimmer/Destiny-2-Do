99b9f75bb130cda4ccd468e098a90941fcb02830
Andy Simmons <rocknswimmer116@gmail.com>
2023-04-12 16:30:49 -0400

    data in local db for dev testing

--- database/list.sql
+++ database/list.sql
@@ -14,16 +14,16 @@ CREATE TABLE missions (
   complete BOOLEAN NOT NULL DEFAULT false,
   category VARCHAR(60) NOT NULL DEFAULT NULL,
   note TEXT DEFAULT NULL,
-  user INTEGER NOT NULL DEFAULT NULL REFERENCES guardians(id)
+  user_id INTEGER NOT NULL DEFAULT NULL REFERENCES guardians(id)
 );
 
 insert into guardians (user_name) values ('andyTest') returning *;
 
-insert into missions (mission, category, user) values ('Vex Calibur/not mission title', 'exotic', 1);
+insert into missions (mission, category, user_id) values ('Vex Calibur/not mission title', 'exotic', 1);
 
-insert into missions (mission, category, user) values ('Beyond Light', 'story', 1);
+insert into missions (mission, category, user_id) values ('Beyond Light', 'story', 1);
 
-insert into missions (mission, category, user) values ('Place for vaiour endgame', 'other', 1);
+insert into missions (mission, category, user_id) values ('Place for vaiour endgame', 'other', 1);
 
-insert into missions (mission, category, user) values ('Crucible Pin', 'weekly', 1);
-insert into missions (mission, category, user) values ('NightFall', 'weekly', 1);
+insert into missions (mission, category, user_id) values ('Crucible Pin', 'weekly', 1);
+insert into missions (mission, category, user_id) values ('NightFall', 'weekly', 1);
