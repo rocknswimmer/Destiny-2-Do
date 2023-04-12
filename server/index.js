3ca839b4d6112c86a794f588d5037051da637b4a
Andy Simmons <rocknswimmer116@gmail.com>
2023-04-12 15:48:53 -0400

    prep for back end

--- server/index.js
+++ server/index.js
@@ -2,7 +2,16 @@ const express = require('express');
 const app = express();
 const compression = require('compression')
 const path = require('path');
+require('dotenv').config()
 
+const Pool = require('pg').Pool;
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+  password: process.env.PGPASSWORD
+});
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
