29c7cd2fc23e5d77c380f1bee228bda7d5927a35
Andy Simmons <rocknswimmer116@gmail.com>
2023-04-13 15:45:06 -0400

    posting new missions

--- server/index.js
+++ server/index.js
@@ -21,13 +21,28 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/missions/:id', (req, res) => {
   const user = req.params.id;
-  pool.query('select * from missions where user_id = $1 and category = $2', [user, 'weekly'], (err, data) => {
+  pool.query('select * from missions where user_id = $1 and category = $2', [user, 'other'], (err, data) => {
     if(err) {
       console.log('error retrieving missions')
     }
     res.send(data)
   })
 })
+
+app.post('/newMission/:id', (req, res) => {
+  const user = req.params.id;
+  const {mission, category} = req.body;
+  // console.log( mission, category, user, 'data before this string')
+  // res.send("testing")
+  pool.query('insert into missions (mission, category, user_id) values ($1, $2, $3)', [mission, category, user], (err, data) => {
+    if(err) {
+      console.log('error creating mission', err)
+    }
+    res.send('mission created')
+  })
+})
+
+
 
 app.listen(3009);
 console.log('Listening on port 3009');
