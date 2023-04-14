f7b4f101f36c1b473218f31992958b640c5706b2
Andy Simmons <rocknswimmer116@gmail.com>
2023-04-14 14:25:13 -0400

    preping front end for info from server

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Feed from './Feed.jsx';
 import Accordion from './Accordion.js'
+import axios from 'axios'
 
 function App() {
   const tempToDos = [
@@ -8,16 +9,46 @@ function App() {
     {task: 'beat story', complete: false},
     {task: 'complete raid', complete: false},
   ]
+  const userId = 1;
+  const [story, setStory] = useState([])
+  const [exotic, setExotic] = useState([])
+  const [weekly, setWeekly] = useState([])
+  const [other, setOther] = useState([])
+
+  // get missions
+
+  //useEffect to set state of missions on load
+
+  //post new mission
 
   return (
     <div>
       <h1>Destiny 2 Do</h1>
       <Accordion
-        title={"test of categories with feeds"}
+        title={"Weekly"}
         content={
           <Feed list={tempToDos}/>
         }
       />
+      <Accordion
+        title={"Exotic"}
+        content={
+          <Feed list={tempToDos}/>
+        }
+      />
+      <Accordion
+        title={"Story"}
+        content={
+          <Feed list={tempToDos}/>
+        }
+      />
+      <Accordion
+        title={"End Game / Other"}
+        content={
+          <Feed list={tempToDos}/>
+        }
+      />
+      <button>Create New Mission</button>
     </div>
   )
 }
--- src/Feed.jsx
+++ src/Feed.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Accordion from './Accordion.js'
+import axios from 'axios'
 
 function Feed({list}) {
+
+  // put functions for add note and mark complete, consider an edit mission function
 
   return (
     <div>
       {list.map((toDo) => {
-        return <div>{toDo.task}</div>
+        return <Accordion
+        title={toDo.task}
+        content={
+          <div>
+            <p>note to go here in a text field with conditions?</p>
+            <button>edit note</button> <button>mark complete/mabye make check box?</button>
+          </div>
+        }
+        />
       })}
     </div>
   )
